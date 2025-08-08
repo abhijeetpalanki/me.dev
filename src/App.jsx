f19d0ecdd8e66a754b76ed249434320467de55f0
abhijeetpalanki <ajpalanki@gmail.com>
2025-08-08 13:31:36 -0500

    About section with gsap animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import CustomCursor from "./components/CustomCursor";
+import AboutSection from "./components/AboutSection";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <HeroSection />
       <CustomCursor />
+      <AboutSection />
     </>
   );
 };
